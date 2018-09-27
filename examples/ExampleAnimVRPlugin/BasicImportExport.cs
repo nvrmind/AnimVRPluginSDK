32b455ee642b1a7757e059219aef18c8efa93bab
Dario Seyb <dario.seyb@gmail.com>
2018-09-26 21:48:52 -0400

    updated plugin sdk for new animvr version

---
+++ examples/ExampleAnimVRPlugin/BasicImportExport.cs
@@ -0,0 +1,18 @@
+﻿using AnimVRFilePlugin;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+[CustomImporter(Extension = "basic")]
+public class BasicImportExport : CustomImporter {
+
+    public override List<PlayableData> Import(string path) {
+        return new List<PlayableData> { new SymbolData() { displayName = Path.GetFileNameWithoutExtension(path) } };
+    }
+
+    public override void Export(StageData stage, string path) {
+        File.WriteAllText(path, "Selected Playable:" + stage.activePlayablePath);
+    }
+}
